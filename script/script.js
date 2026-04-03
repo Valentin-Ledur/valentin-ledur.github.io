5abe079f37d77e525a65b3d374805b3d2959b20e
Valentin-Ledur <valentin.ledur123456789@gmail.com>
2026-04-03 22:09:22 +0200

    test three js

--- script/script.js
+++ script/script.js
@@ -1,60 +1,28 @@
-import * as THREE from 'https://esm.sh/three@0.161.0';
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
-import { GLTFLoader } from 'https://esm.sh/three@0.161.0/examples/jsm/loaders/GLTFLoader.js';
+const canvas = document.getElementById('canvas-container');
 
-const container = document.getElementById('canvas-container');
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 60, canvas.clientWidth / canvas.clientHeight, 0.1, 1000 );
 
-if (container) {
-    const w = container.clientWidth;
-    const h = container.clientHeight;
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( canvas.clientWidth, canvas.clientHeight );
+renderer.setAnimationLoop( animate );
+canvas.appendChild( renderer.domElement );
 
-    const scene = new THREE.Scene();
-    scene.background = new THREE.Color(0x333333);
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
 
-    const camera = new THREE.PerspectiveCamera(60, w / h, 0.1, 1000);
-    camera.position.set(4, 4, 4);
-    camera.lookAt(0, 0, 0);
+camera.position.z = 5;
 
-    const renderer = new THREE.WebGLRenderer({ antialias: true });
-    renderer.setSize(w, h);
-    renderer.setPixelRatio(window.devicePixelRatio);
-    container.appendChild(renderer.domElement);
+function animate( time ) {
 
-    const ambient = new THREE.AmbientLight(0xffffff, 1.5);
-    scene.add(ambient);
-    const sun = new THREE.DirectionalLight(0xffffff, 1);
-    sun.position.set(5, 10, 7);
-    scene.add(sun);
+  cube.rotation.x = time / 2000;
+  cube.rotation.y = time / 1000;
 
-    const cube = new THREE.Mesh(
-        new THREE.BoxGeometry(1, 1, 1),
-        new THREE.MeshStandardMaterial({ color: "blue" })
-    );
-    scene.add(cube);
+  renderer.render( scene, camera );
 
-    const loader = new GLTFLoader();
-    loader.load('model.glb',
-        (gltf) => {
-            scene.remove(cube);
-            scene.add(gltf.scene);
-            console.log("Succès : Modèle GLB affiché");
-        },
-        undefined,
-        (err) => {
-            console.error("Erreur GLTF :", err);
-        }
-    );
-
-    function animate() {
-        requestAnimationFrame(animate);
-        cube.rotation.y += 0.01;
-        renderer.render(scene, camera);
-    }
-    animate();
-
-    window.addEventListener('resize', () => {
-        camera.aspect = container.clientWidth / container.clientHeight;
-        camera.updateProjectionMatrix();
-        renderer.setSize(container.clientWidth, container.clientHeight);
-    });
 }
